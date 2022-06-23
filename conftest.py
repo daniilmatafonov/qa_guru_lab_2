9be0fa441e6ce36eb717b230077de1f50a84b0dc
Daniil Matafonov <daniil.matafonov@altenar.com>
2022-06-23 22:25:13 +0300

    fix

--- conftest.py
+++ conftest.py
@@ -1,8 +1,15 @@
 from selene.support.shared import browser
 import pytest
 
+url = 'https://google.com'
+
 
 @pytest.fixture(scope='session', autouse=True)
-def setup():
-    browser.open('https://google.com')
-    browser.driver.set_window_size(1360, 768)
+def config():
+    browser.config.window_width = 1360
+    browser.config.window_height = 768
+
+
+@pytest.fixture
+def init():
+    browser.open(url)
--- test_google_search.py
+++ test_google_search.py
@@ -1,14 +1,14 @@
 from selene import be, have, by
-from selene.support.shared import browser
+from conftest import *
 
 framework = 'selene'
 
 
-def test_search_selene_is_success(setup):
+def test_search_selene_is_success(init):
     browser.element('[name="q"]').should(be.blank).type(framework).press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 
-def test_search_no_results(setup):
+def test_search_no_results(init):
     browser.element('[name="q"]').should(be.blank).type('hsad12412trw').press_enter()
     browser.element(by.css("#topstuff p")).should(have.text('Your search - hsad12412trw - did not match any documents.'))
